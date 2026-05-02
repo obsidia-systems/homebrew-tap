a801949d0fbfce7cc49b914aea915c140cfe2d86
kolvra <14861488+kolvra@users.noreply.github.com>
2026-05-02 00:12:57 +0000

    envlt 0.2.2

--- Formula/envlt.rb
+++ Formula/envlt.rb
@@ -1,8 +1,8 @@
 class Envlt < Formula
   desc "Local-first encrypted environment variable manager"
   homepage "https://github.com/obsidia-systems/envlt"
-  url "https://github.com/obsidia-systems/envlt/archive/refs/tags/v0.1.7.tar.gz"
-  sha256 "73907590c95b05b170c7bc917b81bf89c4dd0d38e56885159f3081a1d14e622d"
+  url "https://github.com/obsidia-systems/envlt/archive/refs/tags/v0.2.2.tar.gz"
+  sha256 "0dbc5b17d513f66dd82d533762a2626f306af84646b03f3de15a70a283fdba2a"
   license "MIT"
 
   depends_on "rust" => :build
